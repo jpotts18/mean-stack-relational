7ea69af3bc473e04878f345e869dde82e54c2f0f
Mark Gutierrez <mrk.rio.gtrrz@gmail.com>
2015-04-10 18:20:52 +0400

    migrated to express4.12.3
    Signed-off-by: Mark Gutierrez <mrk.rio.gtrrz@gmail.com>

--- config/express.js
+++ config/express.js
@@ -5,6 +5,13 @@ var express = require('express');
 var flash = require('connect-flash');
 var helpers = require('view-helpers');
 var config = require('./config');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
 
 module.exports = function(app, passport) {
 
@@ -16,7 +23,7 @@ module.exports = function(app, passport) {
     app.locals.pretty = true;
 
     //Should be placed before express.static
-    app.use(express.compress({
+    app.use(compression({
         filter: function(req, res) {
             return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
         },
@@ -24,12 +31,12 @@ module.exports = function(app, passport) {
     }));
 
     //Setting the fav icon and static folder
-    app.use(express.favicon());
+    app.use(favicon(config.root + '/public/img/icons/favicon.ico'));
     app.use(express.static(config.root + '/public'));
 
     //Don't use logger for test env
     if (process.env.NODE_ENV !== 'test') {
-        app.use(express.logger('dev'));
+        app.use(logger('dev'));
     }
 
     //Set views path, template engine and default layout
@@ -39,17 +46,21 @@ module.exports = function(app, passport) {
     //Enable jsonp
     app.enable("jsonp callback");
 
-    app.configure(function() {
+    if (process.env.NODE_ENV === 'development') {
         //cookieParser should be above session
-        app.use(express.cookieParser());
+        app.use(cookieParser());
 
         // request body parsing middleware should be above methodOverride
-        app.use(express.urlencoded());
-        app.use(express.json());
-        app.use(express.methodOverride());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+        app.use(methodOverride());
 
         //express/mongo session storage
-        app.use(express.session({ secret: '$uper$ecret$e$$ionKey'}));
+        app.use(session({
+            resave: false, // don't save session if unmodified
+            saveUninitialized: false, // don't create session until something stored
+            secret: '$uper$ecret$e$$ionKey'
+        }));
 
         //connect flash for flash messages
         app.use(flash());
@@ -60,9 +71,6 @@ module.exports = function(app, passport) {
         //use passport session
         app.use(passport.initialize());
         app.use(passport.session());
-
-        //routes should be at the last
-        app.use(app.router);
 
         //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
         app.use(function(err, req, res, next) {
@@ -77,14 +85,14 @@ module.exports = function(app, passport) {
                 error: err.stack
             });
         });
-
+        
         //Assume 404 since no middleware responded
-        app.use(function(req, res, next) {
+        app.use(function(err, req, res, next) {
             res.status(404).render('404', {
                 url: req.originalUrl,
                 error: 'Not found'
             });
         });
-
-    });
+        
+    }
 };
