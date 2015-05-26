933c9641a92b22cf72cfc3167a65230ea94c18aa
Junaid Anwar <junaid.anwar@vizteck.com>
2015-05-26 18:48:30 +0500

    broken: sequelize changes part 1

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,6 +1,6 @@
 var fs        = require('fs');
 var path      = require('path');
-var Sequelize = require('sequelize-mysql').sequelize;
+var Sequelize = require('sequelize');
 var _         = require('lodash');
 var config    = require('./config');
 var db        = {};
