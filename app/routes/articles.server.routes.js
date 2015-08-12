4efe42f17de485d0fb8f1684e1eda3bc4c184b41
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2015-08-12 22:37:10 +0500

    removing iife syntax from routes

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -1,24 +1,24 @@
-(function() {
-  'use strict';
 
-  /**
-   * Module dependencies.
-   */
-  var users = require('../../app/controllers/users'),
-    articles = require('../../app/controllers/articles');
+'use strict';
 
-  module.exports = function(app) {
-    // Article Routes
-    app.route('/articles')
-        .get(articles.all)
-        .post(users.requiresLogin, articles.create);
-    app.route('/articles/:articleId')
-        .get(articles.show)
-        .put(users.requiresLogin, articles.hasAuthorization, articles.update)
-        .delete(users.requiresLogin, articles.hasAuthorization, articles.destroy);
+/**
+* Module dependencies.
+*/
+var users = require('../../app/controllers/users'),
+articles = require('../../app/controllers/articles');
 
-    // Finish with setting up the articleId param
-    // Note: the articles.article function will be called everytime then it will call the next function.
-    app.param('articleId', articles.article);
-  };
-}());
+module.exports = function(app) {
+// Article Routes
+app.route('/articles')
+    .get(articles.all)
+    .post(users.requiresLogin, articles.create);
+app.route('/articles/:articleId')
+    .get(articles.show)
+    .put(users.requiresLogin, articles.hasAuthorization, articles.update)
+    .delete(users.requiresLogin, articles.hasAuthorization, articles.destroy);
+
+// Finish with setting up the articleId param
+// Note: the articles.article function will be called everytime then it will call the next function.
+app.param('articleId', articles.article);
+};
+
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,63 +1,63 @@
-(function() {
-  'use strict';
 
-  /**
-   * Module dependencies.
-   */
-  var passport = require('passport');
+'use strict';
 
-  module.exports = function(app) {
-    // User Routes
-    var users = require('../../app/controllers/users');
+/**
+* Module dependencies.
+*/
+var passport = require('passport');
 
-    // User Routes
-    app.get('/signin', users.signin);
-    app.get('/signup', users.signup);
-    app.get('/signout', users.signout);
-    app.get('/users/me', users.me);
+module.exports = function(app) {
+// User Routes
+var users = require('../../app/controllers/users');
 
-    // Setting up the users api
-    app.post('/users', users.create);
+// User Routes
+app.get('/signin', users.signin);
+app.get('/signup', users.signup);
+app.get('/signout', users.signout);
+app.get('/users/me', users.me);
 
-    // Setting the local strategy route
-    app.post('/users/session', passport.authenticate('local', {
-        failureRedirect: '/signin',
-        failureFlash: true
-    }), users.session);
+// Setting up the users api
+app.post('/users', users.create);
 
-    // Setting the facebook oauth routes
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: ['email', 'user_about_me'],
-        failureRedirect: '/signin'
-    }), users.signin);
+// Setting the local strategy route
+app.post('/users/session', passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
+}), users.session);
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+// Setting the facebook oauth routes
+app.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: ['email', 'user_about_me'],
+    failureRedirect: '/signin'
+}), users.signin);
 
-    // Setting the twitter oauth routes
-    app.get('/auth/twitter', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.signin);
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/signin'
+}), users.authCallback);
 
-    app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+// Setting the twitter oauth routes
+app.get('/auth/twitter', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+}), users.signin);
 
-    // Setting the google oauth routes
-    app.get('/auth/google', passport.authenticate('google', {
-        failureRedirect: '/signin',
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }), users.signin);
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+}), users.authCallback);
 
-    app.get('/auth/google/callback', passport.authenticate('google', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+// Setting the google oauth routes
+app.get('/auth/google', passport.authenticate('google', {
+    failureRedirect: '/signin',
+    scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+    ]
+}), users.signin);
 
-    // Finish with setting up the userId param
-    app.param('userId', users.user);
-  };
-}());
+app.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/signin'
+}), users.authCallback);
+
+// Finish with setting up the userId param
+app.param('userId', users.user);
+};
+
